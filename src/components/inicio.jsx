ecfcf7978590f374cb201ec51c38b33a7fc95eb8
ORW3 <albertoch.ti21@utsjr.edu.mx>
2023-03-29 19:53:06 -0600

    Rollback2

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -1,10 +1,5 @@
 export const Inicio=()=>{
 
-    if (localStorage.getItem("usuario") === null){
-        localStorage.setItem("usuario", "12e1293f-06dc-4de9-9e9d-40afef9e35e9");
-        window.location.reload();
-    }
-
     function click(){
         window.location.href= '/entrar';
         localStorage.setItem("usuario", "12e1293f-06dc-4de9-9e9d-40afef9e35e9");
